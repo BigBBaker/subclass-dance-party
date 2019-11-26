a09d3ea176f5587969e641459e50baf02c7e1d33
BigBBaker <abobkerelaghel@gmail.com>
2019-11-26 19:08:20 +0100

    26/11 sprint 3 Dance Party

--- src/init.js
+++ src/init.js
@@ -15,19 +15,44 @@ $(document).ready(function() {
      * A new object of the given type will be created and added
      * to the stage.
      */
-    var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
 
+     //makeBlinkyDancer
+    var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
+    //console.log(this)
     // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
-
+    //console.log(window[dancerMakerFunctionName])
     // make a dancer with a random position
 
-    var dancer = dancerMakerFunction(
+    var dancer = new dancerMakerFunction(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
     $('body').append(dancer.$node);
   });
-});
+  
+  $('.addJigglyButton').on('click', function(event){
+    
+    var dancer = new jigglyDance(
+      $("body").height() * Math.random(),
+      $("body").width() * Math.random(),
+      Math.random() * 1000
+    );
+    $('body').append(dancer.$node);
+  });
+ 
+  //salsa Dance
+   $('.addSalsalyButton').on('click', function(event){     
+    var dancer = new salsaDance(
+      $("body").height() * Math.random(),
+      $("body").width() * Math.random(),
+      $('body').append("<img src=https://ak7.picdn.net/shutterstock/videos/3193627/thumb/1.jpg>"),
+      Math.random() * 1000
+    );
+    $('body').append(dancer.$node);
+  });
+  }); 
+
+
 
